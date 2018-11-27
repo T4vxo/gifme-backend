dd58cced8d87914f65221577517bdad4568dca7f
Jobook99 <jobo99002@utb.vaxjo.se>
2018-11-27 10:48:20 +0100

    test

---
+++ src/main/java/te/te4/gifmebackend/memebuilder/MemeText.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package te.te4.gifmebackend.memebuilder;
+
+/**
+ *
+ * @author Administratör
+ */
+public class MemeText {
+    private String header; 
+    private String footer; 
+
+    public MemeText(String header, String footer) {
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public void setFooter(String footer) {
+        this.footer = footer;
+    }
+}
--- src/main/java/te/te4/gifmebackend/memebuilder/RequestHandler.java
+++ src/main/java/te/te4/gifmebackend/memebuilder/RequestHandler.java
@@ -5,6 +5,7 @@
  */
 package te.te4.gifmebackend.memebuilder;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import java.io.IOException;
 import java.util.List;
@@ -24,8 +25,9 @@ public class RequestHandler {
     @GET
     @Path("/text/random")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response randomText(@QueryParam("sek") String sek) throws IOException {
-        List<String> res = GetWikiApi.GetWiki(sek);
-        return Response.ok(res.get(1)).build();
+    public Response randomText(@QueryParam("seek") String seek) throws IOException {
+        List<String> res = GetWikiApi.GetWiki(seek);    
+        MemeText text = new MemeText(res.get(0), res.get(1));
+        return Response.ok(JSON.toJSONString(text)).build();
     }
 }
